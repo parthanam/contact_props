9b8adb20abb3c41799983dcd4d97f0be7a5476ce
parthanam <parthnautiyal1016@gmail.com>
2023-06-13 16:32:28 +0530

    second commit

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from "react"
-
+import Contact from "./Contact.js"
 /* Challenge:
 
 - Create a Contact.js component in another file
@@ -10,61 +10,13 @@ import React from "react"
 
 function App() {
     return (
-        <div className="contacts">
-        
-            <div className="contact-card">
-                <img src="./images/mr-whiskerson.png"/>
-                <h3>Mr. Whiskerson</h3>
-                <div className="info-group">
-                    <img src="./images/phone-icon.png" />
-                    <p>(212) 555-1234</p>
-                </div>
-                <div className="info-group">
-                    <img src="./images/mail-icon.png" />
-                    <p>mr.whiskaz@catnap.meow</p>
-                </div>
+       
+            <div className="contacts">
+                <Contact/ >
+                <Contact/ >
+                <Contact/ >
+                <Contact/ >
             </div>
-            
-            <div className="contact-card">
-                <img src="./images/fluffykins.png"/>
-                <h3>Fluffykins</h3>
-                <div className="info-group">
-                    <img src="./images/phone-icon.png" />
-                    <p>(212) 555-2345</p>
-                </div>
-                <div className="info-group">
-                    <img src="./images/mail-icon.png" />
-                    <p>fluff@me.com</p>
-                </div>
-            </div>
-            
-            <div className="contact-card">
-                <img src="./images/felix.png"/>
-                <h3>Felix</h3>
-                <div className="info-group">
-                    <img src="./images/phone-icon.png" />
-                    <p>(212) 555-4567</p>
-                </div>
-                <div className="info-group">
-                    <img src="./images/mail-icon.png" />
-                    <p>thecat@hotmail.com</p>
-                </div>
-            </div>
-            
-            <div className="contact-card">
-                <img src="./images/pumpkin.png"/>
-                <h3>Pumpkin</h3>
-                <div className="info-group">
-                    <img src="./images/phone-icon.png" />
-                    <p>(0800) CAT KING</p>
-                </div>
-                <div className="info-group">
-                    <img src="./images/mail-icon.png" />
-                    <p>pumpkin@scrimba.com</p>
-                </div>
-            </div>
-            
-        </div>
     )
 }
 
